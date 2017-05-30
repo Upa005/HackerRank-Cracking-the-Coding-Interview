360e06be6b47594a68f2f235d566f583971b3ab9
Upasana Ghosh <upasana05ghosh@gmail.com>
2017-05-30 08:07:25 -0700

    try

---
+++ try.py
@@ -0,0 +1,7 @@
+def array_left_rotation(a, n, k):
+  
+
+n, k = map(int, raw_input().strip().split(' '))
+a = map(int, raw_input().strip().split(' '))
+answer = array_left_rotation(a, n, k);
+print ' '.join(map(str,answer))
